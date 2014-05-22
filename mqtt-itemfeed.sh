07f6a6ec73f71cbfecb048aa13624814a8bdf371
Raymond Kuiper <qix@the-wired.net>
2014-05-22 22:33:30 +0200

    Put shell metacharacter (#) in subscibed topic between qoutes (thanks to jpmens).

--- mqtt-itemfeed.sh
+++ mqtt-itemfeed.sh
@@ -4,6 +4,6 @@ ZBXSRV=192.168.60.10
 
 # Start listener for host discoveries
 
-mosquitto_sub -h 192.168.60.20 -v -t zabbix/# | grep --line-buffered -v '^zabbix/clients/' | sed --unbuffered -e 's/^zabbix\///' -e 's/\(.*\)\/\(.*\)/\1\ \2/' | zabbix_sender -r -z $ZBXSRV -i -
+mosquitto_sub -h 192.168.60.20 -v -t 'zabbix/#' | grep --line-buffered -v '^zabbix/clients/' | sed --unbuffered -e 's/^zabbix\///' -e 's/\(.*\)\/\(.*\)/\1\ \2/' | zabbix_sender -r -z $ZBXSRV -i -
 
 
